3d4e0fb19a3a3c1e0decd3ab5011a449064eaf8a
Cristian Giordano <cristian.n.giordano@gmail.com>
2024-10-15 20:41:45 +0100

    chore: admin project

--- apps/admin/src/app/page.tsx
+++ apps/admin/src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Index() {
   return (
     <div className="flex items-center justify-center bg-gray-50 min-h-screen">
-      <h1 className="bg-green-100 p-10 text-gray-600 rounded-lg">
-        Hello from the CLIENT NextJS Application 👋
+      <h1 className="bg-orange-100 p-10 text-gray-600 rounded-lg">
+        Hello from the ADMIN NextJS Application 👋
       </h1>
     </div>
   );
